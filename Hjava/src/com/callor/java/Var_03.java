fc170a0f3b749e292193beaa6418af379487ea45
GOGOYS <qaz75391@naver.com>
2022-03-28 23:48:43 +0900

    double float

---
+++ Hjava/src/com/callor/java/Var_03.java
@@ -0,0 +1,21 @@
+package com.callor.java;
+
+public class Var_03 {
+	public static void main(String[] args) {
+		
+		int intNum1 =33;
+		int intNum2 =77;
+		
+		int intSum = intNum1 + intNum2;
+		int intProduct = intNum1 * intNum2;
+		int intMinus = intNum1 - intNum2;
+		int intDivision = intNum1 / intNum2;
+		
+		System.out.printf("\t%d + %d = %d\n", intNum1, intNum2, intSum);
+		System.out.printf("\t%d * %d = %d\n", intNum1, intNum2, intProduct);
+		System.out.printf("\t%d - %d = %d\n", intNum1, intNum2, intMinus);
+		System.out.printf("\t%d / %d = %d\n", intNum1, intNum2, intDivision);
+		
+		
+	}
+}
---
+++ Hjava/src/com/callor/java/Var_04.java
@@ -0,0 +1,18 @@
+package com.callor.java;
+
+public class Var_04 {
+	public static void main(String[] args) {
+		
+		double douNum1 = Math.random();
+		int intNum2 = (int)Math.random();
+		float floNum3 = (float)Math.random();
+		
+		System.out.println(douNum1);
+		System.out.println(intNum2);
+		System.out.println(floNum3);
+		
+
+		
+	}
+
+}
